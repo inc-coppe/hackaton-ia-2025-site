cba5b6a6004627cb73eefb73d0ea16f601ee1446
O-Jucas <lucasdecamposvargas@gmail.com>
2025-07-08 17:03:30 -0300

    Feat: Adicionando acesso no site.
    Modifiquei o arquivo vite.config.ts para permitir o acesso ao site.

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // permite que o Vite aceite requisições vindas desse host
+    allowedHosts: ["www.hackathon-ia.coppe.ufrj.br"],
+  },
 });
