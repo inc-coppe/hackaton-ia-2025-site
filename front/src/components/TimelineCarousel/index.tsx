b59fe3f59b7aabfe8012598079310316bf1dfd8c
O-Jucas <lucasdecamposvargas@gmail.com>
2025-07-21 12:53:58 -0300

    Fix: Voltando ao estado original
    Consertando um pequeno bug

--- front/src/components/TimelineCarousel/index.tsx
+++ front/src/components/TimelineCarousel/index.tsx
@@ -15,10 +15,10 @@ import Img2 from "../../assets/Sexta 17.png";
 import Img3 from "../../assets/Sábado 18.png";
 import Img4 from "../../assets/Domingo 19.png";
 
-import Img5 from "../../assets/Pré-evento mobile.png";
-import Img6 from "../../assets/Sexta 17 mobile.png";
-import Img7 from "../../assets/Sábado 18 mobile.png";
-import Img8 from "../../assets/Domingo 19 mobile.png";
+import Img5 from "../../assets/Pré-cronograma-mobile (1).png";
+import Img6 from "../../assets/Dia 1-cronograma-mobile (1).png";
+import Img7 from "../../assets/Dia 2-cronograma-mobile (1).png";
+import Img8 from "../../assets/Dia 3-cronograma-mobile (1).png";
 import { NavLink } from "react-router-dom";
 
 const remInPx = parseFloat(getComputedStyle(document.documentElement).fontSize);
