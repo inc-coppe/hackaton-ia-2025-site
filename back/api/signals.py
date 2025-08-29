5eeff217a268e0192e81303244d77be8d3410b27
O-Jucas <lucasdecamposvargas@gmail.com>
2025-08-29 13:35:53 -0300

    feat: sinais
    Criação dos sinais para que o Django ouça quando um perfil for criado.

---
+++ back/api/signals.py
@@ -0,0 +1,30 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import UserProfile, PendingEmail
+
+@receiver(post_save, sender=UserProfile)
+def create_pending_email(sender, instance, created, **kwargs):
+    if created:
+        # só cria se ainda não existir
+        if not PendingEmail.objects.filter(user=instance.user).exists():
+            PendingEmail.objects.create(
+                user=instance.user,
+                full_name=instance.full_name,
+                message=f"""Olá, {full_name}! 
+Obrigado por se inscrever no Hackathon IA 2025!
+
+Para acompanhar todas as atualizações do evento, entre agora no nosso servidor do Discord: Hackathon IA Coppe / UFRJ
+
+Por lá, você encontrará:
+
+Canais de dúvidas e apoio técnico
+
+Networking com outros participantes
+
+Divulgação de novidades e materiais do evento
+
+Não perca nada! Nos vemos lá.
+
+"""
+            )
+            print(f"[TESTE] PendingEmail criado para {instance.full_name}")
