64818a40c9b6ac6b62dbff4b9e2e752c4c32626f
O-Jucas <lucasdecamposvargas@gmail.com>
2025-08-29 13:55:19 -0300

    Fix: Erro de ordem
    Estava enviando o sinal antes de criar o perfil

--- back/api/signals.py
+++ back/api/signals.py
@@ -4,13 +4,12 @@ from .models import UserProfile, PendingEmail
 
 @receiver(post_save, sender=UserProfile)
 def create_pending_email(sender, instance, created, **kwargs):
-    if created:
-        # só cria se ainda não existir
-        if not PendingEmail.objects.filter(user=instance.user).exists():
-            PendingEmail.objects.create(
-                user=instance.user,
-                full_name=instance.full_name,
-                message=f"""Olá, {instance.full_name}! 
+    # Só cria se full_name existir
+    if created and instance.full_name:
+        PendingEmail.objects.create(
+            user=instance.user,
+            full_name=instance.full_name,
+            message=f"""Olá, {instance.full_name}! 
 Obrigado por se inscrever no Hackathon IA 2025!
 
 Para acompanhar todas as atualizações do evento, entre agora no nosso servidor do Discord: Hackathon IA Coppe / UFRJ
@@ -24,7 +23,6 @@ Networking com outros participantes
 Divulgação de novidades e materiais do evento
 
 Não perca nada! Nos vemos lá.
-
 """
-            )
-            print(f"[TESTE] PendingEmail criado para {instance.full_name}")
+        )
+        print(f"[TESTE] PendingEmail criado para {instance.full_name}")
