fb9197123a424710444534477814a21238a57802
O-Jucas <lucasdecamposvargas@gmail.com>
2025-09-02 16:28:30 -0300

    Fix: removendo api
    Foi necessário remover porque a EC2 estava reclamando.

--- back/backend/settings.py
+++ back/backend/settings.py
@@ -64,7 +64,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "api",
     "api.apps.ApiConfig",
     "rest_framework",
     "corsheaders",
