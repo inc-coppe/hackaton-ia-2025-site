b2072186af560387b5088db43f74c9cafde41408
O-Jucas <lucasdecamposvargas@gmail.com>
2025-09-02 17:50:03 -0300

    Fix: Ainda corrigindo problema envio email
    Enviando email automatico

--- back/backend/settings.py
+++ back/backend/settings.py
@@ -181,13 +181,12 @@ SOCIALACCOUNT_PROVIDERS = {
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = smtp.gmail.com
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # ex: inc@incubadora.coppe.ufrj.br
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # senha no .env
-DEFAULT_FROM_EMAIL = f"Hackathon <inc@incubadora.coppe.ufrj.br>"
-
+EMAIL_HOST_USER = lucas.vargas@incubadora.coppe.ufrj.br
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = "Hackathon <lucas.vargas@incubadora.coppe.ufrj.br>"
 
 
 SOCIALACCOUNT_STORE_TOKENS = True
