e209a60823a7e55580a1945a6922f92fc784392f
O-Jucas <lucasdecamposvargas@gmail.com>
2025-09-02 17:54:10 -0300

    Fix: Erro na formatação
    Corrigi um pequeno erro de formtação.

--- back/backend/settings.py
+++ back/backend/settings.py
@@ -181,10 +181,10 @@ SOCIALACCOUNT_PROVIDERS = {
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = smtp.gmail.com
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = lucas.vargas@incubadora.coppe.ufrj.br
+EMAIL_HOST_USER = "lucas.vargas@incubadora.coppe.ufrj.br"
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = "Hackathon <lucas.vargas@incubadora.coppe.ufrj.br>"
 
