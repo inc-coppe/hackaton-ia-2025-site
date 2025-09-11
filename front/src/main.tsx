ddfb7542e359ea2877becb7c7f6980edc1b5b6a9
O-Jucas <lucasdecamposvargas@gmail.com>
2025-09-11 18:12:01 -0300

    Fix: Corrigindo problema de URL
    Corrigindo problema de URl

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -4,6 +4,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const cleanTrackingParams = () => {
+  const url = new URL(window.location.href);
+  ['_gl', '_ga', '_ga_PQWN9WZK16'].forEach(param => url.searchParams.delete(param));
+  window.history.replaceState({}, document.title, url.pathname + url.search);
+};
+
+cleanTrackingParams();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="1087318908065-20g0p6u8iekaahup1qohlrpcantakf6e.apps.googleusercontent.com">
